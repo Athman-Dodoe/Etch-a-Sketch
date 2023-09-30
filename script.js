7e47891fb882f705fa6f9db194cae13f33d5b9e0
Athman-Dodoe <dodoeathman@gmail.com>
2023-09-30 21:40:37 +0300

    created a function to create a new grid

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const gridContainer = document.querySelector('.container');
+const newGrid = document.querySelector('.new-grid');
 
 function createGrid (rows, cols){
     let grid = rows * cols;
@@ -10,8 +11,26 @@ function createGrid (rows, cols){
 
         gridItem.addEventListener('mouseenter', function(){
             gridItem.style.backgroundColor = 'black';
+            
         })
     }
+}
+
+
+newGrid.addEventListener('click', createNewGrid);
+
+function createNewGrid (){
+    gridContainer.innerHTML = '';
+    let newGrid = parseInt(prompt('Enter the number of squares per side for the new grid (maximum 100):'));
+
+    if(!isNaN(newGrid) && newGrid > 0 && newGrid <= 100){
+        createGrid(newGrid, newGrid)
+    }else {
+        alert('Please enter a valid number greater than 0 and less than or equal to 100.');
+    }
+
+    gridContainer.style.gridTemplateColumns = `repeat(${newGrid}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${newGrid}, 1fr)`;
 }
 
 createGrid(16, 16);
